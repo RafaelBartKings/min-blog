2abc9bf174ed90d572716b3a51c96c2f5d7eae1b
Rafael Reis <rafyangtj@gmail.com>
2025-11-25 21:43:29 -0300

    postCreate

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,68 @@
+import { useState, useEffect } from 'react';
+import { db } from '../firebase/config';
+import {
+   collection,
+   query,
+   orderBy,
+   onSnapshot,
+   where
+} from 'firebase/firestore';
+
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+   const [documents, setDocuments] = useState(null);
+   const [error, setError] = useState(null);
+   const [loading, setLoading] = useState(null);
+
+   const [cancelled, setCancelled] = useState(false);
+
+   useEffect(() => {
+      async function loadData() {
+         if (cancelled) return;
+
+         setLoading(true);
+         const collectionRef = collection(db, docCollection);
+
+         try {
+            let q;
+            q = query(collectionRef, orderBy('createdAt', 'desc'));
+
+            const unsubscribe = onSnapshot(
+               q,
+               querySnapshot => {
+                  setDocuments(
+                     querySnapshot.docs.map(doc => ({
+                        id: doc.id,
+                        ...doc.data()
+                     }))
+                  );
+                  setLoading(false); // Defina o loading=false DENTRO do onSnapshot (sucesso)
+                  setError(null);
+               },
+               error => {
+                  // Adicione um handler de erro para onSnapshot
+                  console.log(error);
+                  setError(error.message);
+                  setLoading(false);
+                  setDocuments(null);
+               }
+            );
+
+            // Retorna a função de unsubscribe como cleanup, se o componente desmontar antes do onSnapshot
+            return () => unsubscribe();
+         } catch (error) {
+            console.log(error);
+            setError('Erro ao carregar dados: ' + error.message);
+            setLoading(false);
+            setDocuments(null);
+         }
+      }
+
+      loadData();
+   }, [docCollection, search, uid, cancelled]);
+
+   useEffect(() => {
+      return () => setCancelled(true);
+   }, []);
+
+   return { documents, loading, error };
+};
